28d28fb0a37c7773f4ea690b2bf923930fa0662b
Matthijs86 <boersmamatthijs@gmail.com>
2026-08-27 13:32:09 +0200

    Update service worker to version 35

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 // Verhoog dit nummer wanneer je een nieuwe
 // versie van de app online zet.
 
-const CACHE_NAME = "mijn-recepten-v34";
+const CACHE_NAME = "mijn-recepten-v35";
 
 
 // ======================================
